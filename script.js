9ca8cee1636476b1148a0d49ed906474fbc2b77b
Souvik Guha <svkguha123@gmail.com>
2023-04-01 21:59:44 +0530

    u

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-
 function calculateMinCost() {
+  //your code here
   // read data from input element;
-  var inputData = document.querySelector("#rope-lengths").value;
-  var inputArr = inputData.split(",");
+	var inputData = document.querySelector("#rope-lengths").value;
+	var inputArr = inputData.split(",");
 
-  for (var i = 0; i < inputArr.length; i++) {
-    inputArr[i] = Number(inputArr[i]);
-  }
+	for(var i = 0; i< inputArr.length; i++) {
+		inputArr[i] = Number(inputArr[i]);
+	}
+	var cost = 0;
+	inputArr.sort(function (a,b) { return a-b;});
 
-  var cost = 0;
+	while(inputArr.length > 1) {
+		var newRope = inputArr[0] + inputArr[1];
+		cost += newRope;
 
-  while (inputArr.length > 1) {
-    var newRope = inputArr[0] + inputArr[1];
-    cost += newRope;
-
-    // delete first two element
-    inputArr.splice(0, 2);
-    inputArr.push(newRope);
-  }
-
+		// delete first two element
+		inputArr.splice(0,2);
+		inputArr.push(newRope);
+		//alert(newRope);
+		inputArr.sort(function(a,b) { return a-b});
+	}	
   document.querySelector("#result").textContent = cost;
-}
 
+}  
